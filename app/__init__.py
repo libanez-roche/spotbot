0ecbe9cb7ab671a4291e81d8574163a2aa6e9499
Luis Ibanez <luis.ibanez.li1@roche.com>
2019-09-04 21:59:51 +0200

    Added change command

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+import redis
 from flask_api import FlaskAPI
 from config.env import app_env
 from app.utils.slackhelper import SlackHelper
@@ -15,6 +17,8 @@ def create_app(config_name):
 	app = FlaskAPI(__name__, instance_relative_config=False)
 	app.config.from_object(app_env[config_name])
 	app.config.from_pyfile('../config/env.py')
+	redis_client = redis.from_url(os.environ.get("REDIS_URL"))
+
 
 	@app.route('/spotbot', methods=['POST'])
 	def hackabot():
@@ -31,6 +35,18 @@ def create_app(config_name):
 		if command_text[0] == 'info':
 			response_body = actions.info()
 
+		response = jsonify(response_body)
+		response.status_code = 200
+		return response
+
+	@app.route('/change', methods=['POST'])
+	def change():
+		text = request.data.get('text')
+		slack_uid = request.data.get('user_id')
+		slackhelper = SlackHelper()
+		slack_user_info = slackhelper.user_info(slack_uid)
+		redis_client.set(slack_uid, text)
+		response_body = "Location stored succesfully for user %s on %s" % (slack_uid, text)
 		response = jsonify(response_body)
 		response.status_code = 200
 		return response
