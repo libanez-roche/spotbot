0e3c5cb70ea0832bf54b85bf74cec44d7cbe74bd
ulriche1 <eugen.ulrich@roche.com>
2019-09-04 12:09:39 +0200

    Refactoring

--- app/actions.py
+++ app/actions.py
@@ -13,5 +13,5 @@ class Actions:
 		Return the Available commands in the system and their usage format
 		"""
 		return {
-			'text': 'WELCOME TO THE S2EA HACKATHON: \n '
-					'\n Daniel Butnaru and Luis Ibanez ara vailable for any question during the hackathon \n You can find all the information about the hackathon in this document: https://docs.google.com/document/d/1b9prezD65wZXAm22B1CO1SdUptnCEGVp6zRajRU3Z30/edit?usp=sharing \n Good Luck! \n \n Hackabot Ver: 1.0'}
+			'text': 'WELCOME TO THE SpotBot: \n '
+					'\n Allows to locate where users are sitting.}
