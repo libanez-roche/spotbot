b98bc1e4d4de965b05ebd6abd981197719f9ce86
Luis Ibanez <luis.ibanez.li1@roche.com>
2019-09-04 12:05:30 +0200

    yeha

--- app/actions.py
+++ app/actions.py
@@ -9,8 +9,6 @@ class Actions:
 		self.slackhelper = slackhelper
 
 	def info(self):
-		"""
-		Return the Available commands in the system and their usage format
-		"""
+	
 		return {
-			'text': 'Hello! I am spotbot :smile:
+			'text': 'Hello! I am spotbot :smile:'
